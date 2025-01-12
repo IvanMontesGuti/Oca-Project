458e1ee65c980d7baca43c19b64ed4ffaf786555
Yasir <y.asirbelmaalem30@gmail.com>
2025-01-12 15:06:46 +0100

    commit

--- ocago/src/components/login-form.tsx
+++ ocago/src/components/login-form.tsx
@@ -14,20 +14,19 @@ export function LoginForm({
 }: React.ComponentProps<"div"> & { onClose: () => void }) {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Add your login logic here
-    // After successful login, close the modal
+    //Redirigir al inicio
     onClose();
   };
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden">
-        <CardContent className="grid p-0 md:grid-cols-2">
+        <CardContent className="grid p-0 md:grid-cols-2" >
           <form className="p-6 md:p-8" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Bienvenido de vuelta</h1>
-                <p className="text-balance text-muted-foreground">
+                <h1 className="text-2xl font-bold font-fredoka text-blue-950 ">Bienvenido de vuelta</h1>
+                <p className="text-balance text-muted-foreground font-montserrat">
                   Inicia sesion en tu cuenta de OcaGo!
                 </p>
               </div>
@@ -52,7 +51,7 @@ export function LoginForm({
                 </div>
                 <Input id="password" type="password" required />
               </div>
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full bg-blue-500 hover:bg-blue-900">
                 Iniciar Sesión
               </Button>
               <div className="text-center text-sm">
@@ -65,7 +64,7 @@ export function LoginForm({
           </form>
           <div className="relative hidden bg-muted md:block">
             <img
-              src="/placeholder.svg"
+              src="https://ui.shadcn.com/placeholder.svg"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
