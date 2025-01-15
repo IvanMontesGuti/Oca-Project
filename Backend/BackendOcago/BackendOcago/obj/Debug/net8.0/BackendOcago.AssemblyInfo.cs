a6361db5dc902b76aa9e9852fcff97bd48d8dfd8
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2025-01-15 18:11:59 +0100

    m

--- Backend/BackendOcago/BackendOcago/obj/Debug/net8.0/BackendOcago.AssemblyInfo.cs
+++ Backend/BackendOcago/BackendOcago/obj/Debug/net8.0/BackendOcago.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendOcago")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091dbb2842c2e9df7a3755b279b38b29ca58c8d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8a7cf1e09c0e97680a2d07fd91cc2fa4bd2decd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendOcago")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendOcago")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
