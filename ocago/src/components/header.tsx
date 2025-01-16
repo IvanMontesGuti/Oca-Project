e1b00a609ee061e5da5c9f7aa5389ac179d6faa9
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2025-01-16 18:11:23 +0100

    Clean Look

---
+++ ocago/src/components/header.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Link from 'next/link'
+import Image from 'next/image'
+import { ArrowRight } from 'lucide-react'
+import { useState } from 'react'
+import dynamic from 'next/dynamic'
+import { LoginForm } from '@/components/login-form'
+import { RegisterForm } from '@/components/register-form'
+
+
+const Modal = dynamic(() => import('@/components/modal').then(mod => mod.Modal), { ssr: false })
+
+export function Header() {
+    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+      const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
+    
+  return (
+    <>
+    <nav className="container mx-auto px-4 py-6 flex justify-between items-center">
+        <div className="flex items-center gap-2 ">
+        <Image
+                  src="/images/logo.svg"
+                  alt="logo"
+                  width={50}
+                  height={50}
+                  
+                />
+          <span className="text-white text-2xl font-fredoka flex items-center gap-2">
+            OcaGo! <ArrowRight className="h-5 w-5" />
+          </span>
+        </div>
+        <div className="flex gap-4">
+        
+          <Link
+            href="#faq"
+            className="text-white hover:text-gray-200 transition-colors font-montserrat"
+          >
+            Preguntas frecuentes
+          </Link>
+          <button
+            onClick={() => setIsLoginModalOpen(true)}
+            className="text-white hover:text-gray-200 transition-colors font-montserrat"
+          >
+            Inicia Sesión
+          </button>
+          <button
+            onClick={() => setIsRegisterModalOpen(true)}
+            className="text-white hover:text-gray-200 transition-colors font-montserrat"
+          >
+            Regístrate
+          </button>
+        </div>
+      </nav>
+      <Modal isOpen={isLoginModalOpen} onClose={() => setIsLoginModalOpen(false)}>
+              <LoginForm onClose={() => setIsLoginModalOpen(false)} />
+            </Modal>
+            <Modal isOpen={isRegisterModalOpen} onClose={() => setIsRegisterModalOpen(false)}>
+              <RegisterForm onClose={() => setIsRegisterModalOpen(false)} />
+            </Modal>
+      </>
+  );
+}
---
+++ ocago/src/components/preguntas.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+export function Preguntas() {
+  return (
+    <>
+    <div className="mt-auto m-right-8 mb-8 md:-right-16 w-full md:w-1/2 mx-auto text-white hover:text-gray-200 transition-colors font-montserrat">
+        <h1 className='text-3xl	font-size: 1.875rem'>Preguntas Frecuentes</h1>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger id="faq">¿Como se juega?</AccordionTrigger>
+            <AccordionContent>
+              El juego de la Oca es un juego de mesa tradicional español muy popular. Se juega en un tablero con un recorrido de casillas numeradas, y el objetivo es llegar al final del recorrido antes que los demás jugadores.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger id="faq">Objetivo del juego</AccordionTrigger>
+            <AccordionContent>
+            El objetivo es llegar a la casilla número 63 antes que los demás jugadores.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger id="faq">Jugadores</AccordionTrigger>
+            <AccordionContent>
+            Puede jugar de 2 a 6 jugadores. Aunque en esta versión online, el número de jugadores es únicamente de 2.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger id="faq">Desarrollo del juego</AccordionTrigger>
+            <AccordionContent>
+            En cada turno, el jugador tira un dado y avanza el número de casillas que indique. Algunas casillas tienen efectos especiales.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-5">
+            <AccordionTrigger id="faq">Reglas especiales</AccordionTrigger>
+            <AccordionContent>
+            Si un jugador cae en una casilla ocupada por otro, debe retroceder hasta una casilla anterior.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+      </>
+  );
+}
