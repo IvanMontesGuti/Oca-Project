ded0d73c12425f687cbd0344f9368d0fae003403
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2025-01-28 15:54:20 +0100

    s

--- BackendOcago/BackendOcago/Models/Database/Repositories/FriendshipRepository.cs
+++ BackendOcago/BackendOcago/Models/Database/Repositories/FriendshipRepository.cs
@@ -38,6 +38,7 @@ namespace BackendOcago.Models.Database.Repositories
                 .ToListAsync();
         }
 
+        
 
         public async Task<IEnumerable<Friendship>> GetAcceptedFriendshipsAsync(long userId)
         {
